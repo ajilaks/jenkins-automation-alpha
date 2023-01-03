5a2a4a70d0c5751e9913604067264c542526dbdc
aji.laksono <aji.laksono@gdn-commerce.com>
2023-01-03 08:02:50 +0700

    add groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,3 +2,10 @@ def build(String command) {
     sh("chmod +x mvnw")
     echo("executing ./mvnw ${command}")
 }
+
+def buildMultiple(List commands) {
+    for (command in commands) {
+        echo("executing ./mvnw ${command}")
+    }
+
+}
